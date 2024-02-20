769ef1dab1db9a3d4ae1154738f465363395a4aa
patrick.ryanto@outlook.com <patrick.ryanto@outlook.com>
2024-02-20 18:47:52 +0700

    rm vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-    adapter: vercel({
-        imageService: true,
-        devImageService: "squoosh",
-    }),
+
     integrations: [tailwind()],
 });
