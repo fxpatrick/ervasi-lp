e015268c7a013795b0a4b1f8a3b2c9afa1a84ccf
patrick.ryanto@outlook.com <patrick.ryanto@outlook.com>
2024-02-20 18:38:59 +0700

    add adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-	adapter: vercel({
-		imageService: true,
-		devImageService: 'squoosh',
-	  }),
-  integrations: [tailwind()]
+    adapter: vercel({
+        imageService: true,
+        devImageService: "squoosh",
+    }),
+    integrations: [tailwind()],
 });
